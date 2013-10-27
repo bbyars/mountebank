4a601d98c77ce66a142b272161593c7cf461af1b
Brandon Byars <brandon.byars@gmail.com>
2013-10-27 16:43:50 -0500

    Renaming lib->src

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
@@ -11,10 +10,8 @@ module.exports = function(grunt) {
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    // Default task(s).
     grunt.registerTask('default', ['jshint']);
 
 };
