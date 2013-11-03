0d2dd2968677d684a08b3dabe72f67f9cdb8969e
Brandon Byars <brandon.byars@gmail.com>
2013-11-03 15:17:40 -0600

    Removed spurious comment; fixed lint

--- server/test/util/portsTest.js
+++ server/test/util/portsTest.js
@@ -11,7 +11,7 @@ describe('ports', function () {
         });
 
         it('should not accept floats', function () {
-            assert(!ports.isValidPortNumber(123.1))
+            assert(!ports.isValidPortNumber(123.1));
         });
 
         it('should be in the correct range', function () {
@@ -23,26 +23,3 @@ describe('ports', function () {
     });
 });
 
-
-//var testCase = require('nodeunit').testCase,
-//    spawn = require('child_process').spawn,
-
-//
-//    'isPortInUse detects used port': function (test) {
-//        // Not a great test - assumes both that you have netcat installed,
-//        // and that port 3333 is currently unused.  Any better ideas?
-//        var netcat = spawn('nc', ['-l', 3333]);
-//        isPortInUse(3333, function (isInUse) {
-//            test.ok(isInUse);
-//            netcat.kill();
-//            test.done();
-//        });
-//    },
-//
-//    'isPortInUse detects unused port': function (test) {
-//        isPortInUse(3333, function (isInUse) {
-//            test.notOk(isInUse);
-//            test.done();
-//        });
-//    }
-//});
