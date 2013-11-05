db646ec06b3d2b215a737c816fd56d1d69c56339
Brandon Byars <brandon.byars@gmail.com>
2013-11-04 23:38:23 -0600

    Additional validations; removed need for sinon

--- server/src/controllers/impostersController.js
+++ server/src/controllers/impostersController.js
@@ -3,25 +3,60 @@
 var ports = require('../util/ports'),
     Imposter = require('../models/imposter');
 
-function create(protocols, imposters) {
-    return {
-        get: function get (request, response) {
-            var result = imposters.reduce(function (accumulator, imposter) {
-               return accumulator.concat(imposter.hypermedia(response));
-            }, []);
-            response.send({ imposters: result });
-        },
+function create (protocols, imposters) {
 
-        post: function post (request, response) {
-            var port = request.body.port,
-                protocol = request.body.protocol,
-                imposter = Imposter.create(protocol, port);
+    function validationErrorsFor (protocol, port) {
+        var errors = [];
 
+        if (!port) {
+            errors.push({
+                code: "missing field",
+                message: "'port' is a required field"
+            });
+        }
+        if (!protocol) {
+            errors.push({
+                code: "missing field",
+                message: "'protocol' is a required field"
+            });
+        }
+        if (protocol && !protocols[protocol]) {
+            errors.push({
+                code: "unsupported protocol",
+                message: "Of course I can support the " + protocol + " protocol.  I have it on good authority that in just a few days, my legion of open source contributors will have it ready for you!"
+            });
+        }
+        return errors;
+    }
+
+    function get (request, response) {
+        var result = imposters.reduce(function (accumulator, imposter) {
+            return accumulator.concat(imposter.hypermedia(response));
+        }, []);
+        response.send({ imposters: result });
+    }
+
+    function post (request, response) {
+        var protocol = request.body.protocol,
+            port = request.body.port,
+            errors = validationErrorsFor(protocol, port);
+
+        if (errors.length > 0) {
+            response.statusCode = 400;
+            response.send({errors: errors});
+        }
+        else {
+            var imposter = Imposter.create(protocol, port);
             imposters.push(imposter);
             response.setHeader('Location', response.absoluteUrl('/servers/' + port));
             response.statusCode = 201;
             response.send(imposter.hypermedia(response));
         }
+    }
+
+    return {
+        get: get,
+        post: post
     };
 }
 
--- server/test/controllers/impostersControllerTest.js
+++ server/test/controllers/impostersControllerTest.js
@@ -2,25 +2,28 @@
 
 var Controller = require('../../src/controllers/impostersController'),
     assert = require('assert'),
-    sinon = require('sinon'),
     Imposter = require('../../src/models/imposter');
 
 describe('ImpostersController', function () {
 
+    var response;
+
+    beforeEach(function () {
+        response = {
+            headers: {},
+            send: function (body) { this.body = body; },
+            setHeader: function (key, value) { this.headers[key] = value; },
+            absoluteUrl: function (endpoint) { return 'http://localhost' + endpoint; }
+        };
+    });
+
     describe('#get', function () {
-        var response, send;
-
-        beforeEach(function () {
-            send = sinon.spy();
-            response = {send: send};
-        });
-
         it('should send an empty array if no servers', function () {
             var controller = Controller.create({}, []);
 
             controller.get({}, response);
 
-            assert(send.calledWith({imposters: []}));
+            assert.deepEqual(response.body, {imposters: []});
         });
 
         it('should send hypermedia for all servers', function () {
@@ -30,54 +33,87 @@ describe('ImpostersController', function () {
 
             controller.get({}, response);
 
-            assert(send.calledWith({imposters: ["firstHypermedia", "secondHypermedia"]}));
+            assert.deepEqual(response.body, {imposters: ["firstHypermedia", "secondHypermedia"]});
         });
     });
 
     describe('#post', function () {
-        var request, response, setHeader, send;
+        var request;
 
         beforeEach(function () {
             request = { body: {} };
-            send = sinon.spy();
-            setHeader = sinon.spy();
-            response = {
-                send: send,
-                setHeader: setHeader,
-                absoluteUrl: function (endpoint) { return 'http://localhost' + endpoint; }
-            };
         });
 
         it('should return a 201 with the Location header set', function () {
-            var controller = Controller.create({}, []);
-            request.body.port = 8000;
+            var controller = Controller.create({'http': {}}, []);
+            request.body = { port: 8000, protocol: 'http' };
 
             controller.post(request, response);
 
-            assert(setHeader.calledWith('Location', 'http://localhost/servers/8000'));
+            assert(response.headers.Location, 'http://localhost/servers/8000');
             assert.strictEqual(response.statusCode, 201);
         });
 
         it('should return imposter hypermedia', function () {
-            var controller = Controller.create({}, []),
+            var controller = Controller.create({'http': {}}, []),
                 imposter = Imposter.create('http', 8000);
-            request.body.protocol = 'http';
-            request.body.port = 8000;
+            request.body = { port: 8000, protocol: 'http' };
 
             controller.post(request, response);
 
-            assert(send.calledWith(imposter.hypermedia(response)));
+            assert.deepEqual(response.body, imposter.hypermedia(response));
         });
 
         it('should add new imposter to list of all imposters', function () {
             var imposters = [],
-                controller = Controller.create({}, imposters);
-            request.body.protocol = 'http';
-            request.body.port = 8000;
+                controller = Controller.create({'http': {}}, imposters);
+            request.body = { port: 8000, protocol: 'http' };
 
             controller.post(request, response);
 
             assert.strictEqual(imposters.length, 1);
+        });
+
+        it('should return a 400 for a missing port', function () {
+            var controller = Controller.create({'http': {}}, []);
+            request.body = { protocol: 'http' };
+
+            controller.post(request, response);
+
+            assert.strictEqual(response.statusCode, 400);
+            assert.deepEqual(response.body, {
+                errors: [{
+                    code: "missing field",
+                    message: "'port' is a required field"
+                }]
+            });
+        });
+
+        it('should return a 400 for a missing protocol', function () {
+            var controller = Controller.create({'http': {}}, []);
+            request.body = { port: 8000 };
+
+            controller.post(request, response);
+
+            assert.strictEqual(response.statusCode, 400);
+            assert.deepEqual(response.body, {
+                errors: [{
+                    code: "missing field",
+                    message: "'protocol' is a required field"
+                }]
+            });
+        });
+
+        it('should return a 400 for unsupported protocols', function () {
+            var protocols = {},
+                controller = Controller.create(protocols, []);
+            request.body = { port: 8000, protocol: 'unsupported' };
+
+            controller.post(request, response);
+
+            assert.strictEqual(response.statusCode, 400);
+            assert.strictEqual(response.body.errors.length, 1);
+            assert.strictEqual(response.body.errors[0].code, 'unsupported protocol');
         });
     });
 });
