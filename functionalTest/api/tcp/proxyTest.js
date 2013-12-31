dd1e5be6006d70759d9d32a3fc4c4e3442c779c8
Brandon Byars <brandon.byars@gmail.com>
2013-12-30 18:15:34 -0600

    Refactored common server code between http and https

--- functionalTest/api/tcp/proxyTest.js
+++ functionalTest/api/tcp/proxyTest.js
@@ -5,7 +5,7 @@ var assert = require('assert'),
     api = require('../api'),
     promiseIt = require('../../testHelpers').promiseIt,
     port = api.port + 1,
-    timeout = parseInt(process.env.SLOW_TEST_TIMEOUT_MS || 2000);
+    timeout = parseInt(process.env.SLOW_TEST_TIMEOUT_MS || 3000);
 
 describe('tcp proxy', function () {
     this.timeout(timeout);
