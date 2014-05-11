eb45179e904fe83adf689ba38cb7df933dc90f3b
Brandon Byars <brandon.byars@gmail.com>
2014-05-11 13:47:18 -0500

    Added delete capability

--- src/public/scripts/imposters.js
+++ src/public/scripts/imposters.js
@@ -2,21 +2,34 @@
 /*global $:false */
 /*global document:false */
 
+function setResponse (data) {
+    $('#api-response').text(JSON.stringify(data, null, 4));
+}
+
+function request (verb, path) {
+    $('#api-request').text(verb + ' ' + path);
+    $.ajax({
+        url: path,
+        type: verb,
+        success: setResponse
+    });
+}
+
 $(document).ready(function () {
     $('a').on('click', function () {
         var link = $(this),
-            imposter = link.closest('tr').attr('id').replace('imposter-', ''),
+            row = link.closest('tr'),
+            imposter = row.attr('id').replace('imposter-', ''),
+            url = '/imposters/' + imposter,
             action = link.attr('class').replace('-icon', '');
 
         switch (action) {
             case 'inspect':
-                var url = '/imposters/' + imposter;
-                $('#api-request').text('GET ' + url);
-                $.get(url, function (data) {
-                    $('#api-response').text(JSON.stringify(data, null, 4));
-                });
+                request('GET', url);
                 break;
             case 'delete':
+                request('DELETE', url);
+                row.fadeOut(500, row.remove);
                 break;
             case 'add':
                 break;
