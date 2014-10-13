35f642ed0b659c1bac5f235876b15dba5cde5e81
Brandon Byars <brandon.byars@gmail.com>
2014-10-13 08:21:59 -0500

    Adding functional tests for fake 'foo' protocol

---
+++ functionalTest/api/foo/fooImposterTest.js
@@ -0,0 +1,100 @@
+'use strict';
+
+var assert = require('assert'),
+    api = require('../api'),
+    tcp = require('../tcp/tcpClient'),
+    promiseIt = require('../../testHelpers').promiseIt,
+    port = api.port + 1,
+    timeout = parseInt(process.env.SLOW_TEST_TIMEOUT_MS || 2000);
+
+describe('foo imposter', function () {
+    this.timeout(timeout);
+
+    describe('POST /imposters/:id', function () {
+        promiseIt('should auto-assign port if port not provided', function () {
+            var request = { protocol: 'foo', name: this.name };
+
+            return api.post('/imposters', request).then(function (response) {
+                assert.strictEqual(response.statusCode, 201);
+                assert.ok(response.body.port > 0);
+            }).finally(function () {
+                return api.del('/imposters');
+            });
+        });
+    });
+
+    describe('GET /imposters/:id', function () {
+        promiseIt('should provide access to all requests', function () {
+            var request = { protocol: 'foo', port: port, name: this.name };
+
+            return api.post('/imposters', request).then(function () {
+                return tcp.fireAndForget('first', port);
+            }).then(function () {
+                return tcp.fireAndForget('second', port);
+            }).then(function () {
+                return api.get('/imposters/' + port);
+            }).then(function (response) {
+                var requests = response.body.requests.map(function (request) {
+                    return request.data;
+                });
+                assert.deepEqual(requests, ['first', 'second']);
+            }).finally(function () {
+                return api.del('/imposters');
+            });
+        });
+
+        promiseIt('should return list of stubs in order', function () {
+            var first = { responses: [{ is: { data: '1' }}]},
+                second = { responses: [{ is: { data: '2' }}]},
+                request = { protocol: 'foo', port: port, stubs: [first, second], name: this.name };
+
+            return api.post('/imposters', request).then(function () {
+                return api.get('/imposters/' + port);
+            }).then(function (response) {
+                assert.strictEqual(response.statusCode, 200);
+                assert.deepEqual(response.body.stubs, [
+                    { responses: [{ is: { data: '1' } }] },
+                    { responses: [{ is: { data: '2' } }] }
+                ]);
+            }).finally(function () {
+                return api.del('/imposters');
+            });
+        });
+
+        promiseIt('should record matches against stubs', function () {
+            var stub = { responses: [{ is: { data: '1' }}, { is: { data: '2' }}]},
+                request = { protocol: 'foo', port: port, stubs: [stub], name: this.name };
+
+            return api.post('/imposters', request).then(function () {
+                return tcp.send('first', port);
+            }).then(function () {
+                return tcp.send('second', port);
+            }).then(function () {
+                return api.get('/imposters/' + port);
+            }).then(function (response) {
+                var stubs = JSON.stringify(response.body.stubs),
+                    withTimeRemoved = stubs.replace(/"timestamp":"[^"]+"/g, '"timestamp":"NOW"'),
+                    withClientPortRemoved = withTimeRemoved.replace(/"requestFrom":"[:\.\d]+"/g, '"requestFrom":"HERE"'),
+                    actualWithoutEphemeralData = JSON.parse(withClientPortRemoved);
+
+                assert.deepEqual(actualWithoutEphemeralData, [{
+                    responses: [{ is: { data: '1' } }, { is: { data: '2' } }],
+                    matches: [
+                        {
+                            timestamp: 'NOW',
+                            request: { requestFrom: 'HERE', data: 'first' },
+                            response: { data: '1' }
+                        },
+                        {
+                            timestamp: 'NOW',
+                            request: { requestFrom: 'HERE', data: 'second' },
+                            response: { data: '2' }
+                        }
+                    ]
+                }]);
+            }).finally(function () {
+                return api.del('/imposters');
+            });
+        });
+    });
+});
---
+++ functionalTest/api/foo/fooStubTest.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var assert = require('assert'),
+    api = require('../api'),
+    promiseIt = require('../../testHelpers').promiseIt,
+    port = api.port + 1,
+    timeout = parseInt(process.env.SLOW_TEST_TIMEOUT_MS || 2000),
+    tcp = require('../tcp/tcpClient');
+
+describe('foo imposter', function () {
+    this.timeout(timeout);
+
+    describe('POST /imposters with stubs', function () {
+        promiseIt('should return stubbed response', function () {
+            var stub = {
+                    predicates: [{ equals: { data: 'client' } }],
+                    responses: [{ is: { data: 'server' } }]
+                },
+                request = { protocol: 'foo', port: port, stubs: [stub], name: this.name };
+
+            return api.post('/imposters', request).then(function (response) {
+                assert.strictEqual(response.statusCode, 201, JSON.stringify(response.body));
+
+                return tcp.send('client', port);
+            }).then(function (response) {
+                assert.strictEqual(response.toString(), 'server');
+            }).finally(function () {
+                return api.del('/imposters');
+            });
+        });
+
+        promiseIt('should allow a sequence of stubs as a circular buffer', function () {
+            var stub = {
+                    predicates: [{ equals: { data: 'request' } }],
+                    responses: [{ is: { data: 'first' }}, { is: { data: 'second' }}]
+                },
+                request = { protocol: 'foo', port: port, stubs: [stub], name: this.name };
+
+            return api.post('/imposters', request).then(function () {
+                return tcp.send('request', port);
+            }).then(function (response) {
+                assert.strictEqual(response.toString(), 'first');
+
+                return tcp.send('request', port);
+            }).then(function (response) {
+                assert.strictEqual(response.toString(), 'second');
+
+                return tcp.send('request', port);
+            }).then(function (response) {
+                assert.strictEqual(response.toString(), 'first');
+
+                return tcp.send('request', port);
+            }).then(function (response) {
+                assert.strictEqual(response.toString(), 'second');
+            }).finally(function () {
+                return api.del('/imposters');
+            });
+        });
+
+        promiseIt('should only return stubbed response if matches complex predicate', function () {
+            var stub = {
+                    responses: [{ is: { data: 'MATCH' }}],
+                    predicates: [
+                        { equals: { data: 'test' } },
+                        { startsWith: { requestFrom: '127.0.0.1' } }
+                    ]
+                },
+                request = { protocol: 'foo', port: port, stubs: [stub] };
+
+            return api.post('/imposters', request).then(function (response) {
+                assert.strictEqual(response.statusCode, 201, JSON.stringify(response.body));
+                return tcp.send('not test', port, 100);
+            }).then(function (response) {
+                assert.strictEqual(response.toString(), '');
+
+                return tcp.send('test', port, 100);
+            }).then(function (response) {
+                assert.strictEqual(response.toString(), 'MATCH');
+            }).finally(function () {
+                return api.del('/imposters');
+            });
+        });
+
+        promiseIt('should allow proxy stubs', function () {
+            var proxyPort = port + 1,
+                proxyStub = { responses: [{ is: { data: 'PROXIED' } }] },
+                proxyRequest = { protocol: 'foo', port: proxyPort, stubs: [proxyStub], name: this.name + ' PROXY' },
+                stub = { responses: [{ proxy: { to: { host: 'localhost', port:  proxyPort } } }] },
+                request = { protocol: 'foo', port: port, stubs: [stub], name: this.name + ' MAIN' };
+
+            return api.post('/imposters', proxyRequest).then(function () {
+                return api.post('/imposters', request);
+            }).then(function () {
+                return tcp.send('request', port);
+            }).then(function (response) {
+                assert.strictEqual(response.toString(), 'PROXIED');
+            }).finally(function () {
+                return api.del('/imposters');
+            });
+        });
+    });
+});
