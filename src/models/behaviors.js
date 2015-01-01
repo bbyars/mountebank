b01b11b33180a351e452ba4d2aa7f0ac5e0c49dc
Brandon Byars <brandon.byars@gmail.com>
2014-12-31 22:09:00 -0600

    Initial implementation of decorate behavior

--- src/models/behaviors.js
+++ src/models/behaviors.js
@@ -1,9 +1,32 @@
 'use strict';
 
-var Q = require('q');
+var helpers = require('../util/helpers'),
+    errors = require('../util/errors'),
+    Q = require('q');
 
 function wait (response, milliseconds) {
     return response.delay(milliseconds);
+}
+
+function decorate (request, responsePromise, fn, logger) {
+    return responsePromise.then(function (response) {
+        /* jshint evil: true */
+        var scope = helpers.clone(request),
+            injected = '(' + fn + ')(scope, response, logger);';
+
+        try {
+            // support returning a value
+            eval(injected);
+            return Q(response);
+        }
+        catch (error) {
+            logger.error("injection X=> " + error);
+            logger.error("    full source: " + JSON.stringify(injected));
+            logger.error("    request: " + JSON.stringify(scope));
+            logger.error("    response: " + JSON.stringify(response));
+            return Q.reject(errors.InjectionError('invalid response injection', { source: injected, data: error.message }));
+        }
+    });
 }
 
 function execute (response, behaviors, logger) {
@@ -18,11 +41,15 @@ function execute (response, behaviors, logger) {
     if (behaviors.wait) {
         result = wait(result, behaviors.wait);
     }
+    if (behaviors.decorate) {
+        result = decorate({}, result, behaviors.decorate, logger);
+    }
 
     return result;
 }
 
 module.exports = {
     wait: wait,
+    decorate: decorate,
     execute: execute
 };
