0ebeb7e8734f5ab0e1aeb66a912985000f7fb893
Brandon Byars <brandon.byars@gmail.com>
2015-01-10 11:53:20 -0600

    Made release notices on home page dynamic

--- test/controllers/homeControllerTest.js
+++ test/controllers/homeControllerTest.js
@@ -1,13 +1,14 @@
 'use strict';
 
-var controller = require('../../src/controllers/homeController'),
+var Controller = require('../../src/controllers/homeController'),
     assert = require('assert'),
     FakeResponse = require('../fakes/fakeResponse');
 
 describe('homeController', function () {
     describe('#get', function () {
         it('should return base hypermedia', function () {
-            var response = FakeResponse.create();
+            var response = FakeResponse.create(),
+                controller = Controller.create([]);
 
             controller.get({}, response);
 
