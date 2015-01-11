a581ac59281a60e57a9e2da9242e4002ef2b85fe
Brandon Byars <brandon.byars@gmail.com>
2015-01-11 08:58:30 -0600

    Always stop mb even if functional tests fail https://github.com/bbyars/mountebank/issues/7

--- Gruntfile.js
+++ Gruntfile.js
@@ -231,8 +231,30 @@ module.exports = function (grunt) {
         }
     });
 
+    var defaultWarnHandler = grunt.fail.warn;
+
+    grunt.registerTask('try', 'Support continuing beyond failure while still capturing failures', function () {
+        grunt.option('force', true);
+        grunt.fail.warn = function () {
+            grunt.config.set('mb:warning-issued', true);
+            defaultWarnHandler.apply(grunt, Array.prototype.slice.call(arguments));
+        };
+    });
+
+    grunt.registerTask('finally', 'Restore grunt to stop on failure', function () {
+        grunt.option('force', false);
+        grunt.fail.warn = defaultWarnHandler;
+    });
+
+    grunt.registerTask('checkForErrors', 'fail build if any steps executed in a try block failed', function () {
+        if(grunt.config('mb:warning-issued')) {
+            grunt.fail.warn('Failing from previous errors');
+        }
+    });
+
     grunt.registerTask('test:unit', 'Run the unit tests', ['mochaTest:unit']);
-    grunt.registerTask('test:functional', 'Run the functional tests', ['dist', 'mb:restart', 'mochaTest:functional', 'mb:stop']);
+    grunt.registerTask('test:functional', 'Run the functional tests',
+        ['dist', 'mb:restart', 'try', 'mochaTest:functional', 'finally', 'mb:stop', 'checkForErrors']);
     grunt.registerTask('test:performance', 'Run the performance tests', ['mochaTest:performance']);
     grunt.registerTask('test', 'Run all non-performance tests', ['test:unit', 'test:functional']);
     grunt.registerTask('coverage', 'Generate code coverage', ['mochaTest:coverage']);
