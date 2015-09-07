0459e88bbb1d902f57c2a7f3c4de7f02487442cd
Brandon Byars <brandon.byars@gmail.com>
2015-09-07 17:57:01 -0500

    fixing linting problem

--- functionalTest/compatibility.js
+++ functionalTest/compatibility.js
@@ -3,7 +3,7 @@
 // for new Buffer([0,1,2,3]).toJSON()
 //      - v0.10 returns [0.1,2,3]
 //      - v0.12 returns { type: 'Buffer', data: [0,1,2,3] }
-function bufferJSON(buffer) {
+function bufferJSON (buffer) {
     var result = buffer.toJSON();
     return result.data ? result.data : result;
 }
