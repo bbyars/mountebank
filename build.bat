555c6e26003f88e98e41487a08b4ccc34202dc09
Brandon Byars <brandon.byars@gmail.com>
2015-09-10 17:20:35 -0500

    Adding node v4.0 CI for Windows

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 @echo off
 
-REM The jsdom installation included in npm install requires python 2.7,
-REM and probably some other build tools, to install correctly on Windows
+REM For node v0.12 and below, the jsdom installation included in npm install requires
+REM python 2.7, REM and probably some other build tools, to install correctly on Windows
 
+call node scripts/fixDependencies
 call npm install
 set MB_PORT=3535
 node node_modules\grunt-cli\bin\grunt %*
