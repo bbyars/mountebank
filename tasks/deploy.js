73cffcb07f553080accdf34c5f437994c5f59701
Brandon Byars <brandon.byars@gmail.com>
2015-09-13 09:23:08 -0500

    Finished migration of all deploy activities to .travis.yml

---
+++ tasks/deploy.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var run = require('./run').run,
+    deploy = process.env.MB_DEPLOY || false,
+    publish = process.env.MB_PUBLISH || false;
+
+module.exports = function (grunt) {
+
+    grunt.registerTask('deployS3', 'Deploy artifacts to S3', function () {
+        if (!deploy) {
+            return;
+        }
+
+        var done = this.async();
+        run('scripts/deploy/deployS3', []).done(function () { done(); });
+    });
+
+    grunt.registerTask('deployHeroku', 'Deploy artifacts to Heroku', function () {
+        if (!deploy) {
+            return;
+        }
+
+        var done = this.async();
+        run('scripts/deploy/deployHeroku', [publish]).done(function () { done(); });
+    });
+
+    grunt.registerTask('deployNpm', 'Deploy artifacts to npm', function () {
+        if (!deploy) {
+            return;
+        }
+
+        var done = this.async();
+        run('scripts/deploy/deployNpm', [publish]).done(function () { done(); });
+    });
+};
--- tasks/dist.js
+++ tasks/dist.js
@@ -1,29 +1,10 @@
 'use strict';
 
 var fs = require('fs-extra'),
-    spawn = require('child_process').spawn,
     os = require('os'),
-    Q = require('q'),
     thisPackage = require('../package.json'),
-    version = process.env.MB_VERSION || thisPackage.version;
-
-function run (command, args, options) {
-    var deferred = Q.defer(),
-        proc = spawn(command, args, options);
-
-    proc.stdout.on('data', function (data) { console.log(data.toString('utf8').trim()); });
-    proc.stderr.on('data', function (data) { console.error(data.toString('utf8').trim()); });
-
-    proc.on('close', function (exitCode) {
-        if (exitCode !== 0) {
-            process.exit(exitCode);
-        }
-
-        deferred.resolve();
-    });
-
-    return deferred.promise;
-}
+    version = process.env.MB_VERSION || thisPackage.version,
+    run = require('./run').run;
 
 module.exports = function (grunt) {
 
