db17f507bdc76ff8cc82afe66713fbb15d1fb792
Brandon Byars <brandon.byars@gmail.com>
2015-09-13 21:38:19 -0600

    Troubleshooting broken tests on Windows

--- functionalTest/mb.js
+++ functionalTest/mb.js
@@ -7,12 +7,18 @@ var Q = require('q'),
     mbPath = process.env.MB_EXECUTABLE || path.normalize(__dirname + '/../bin/mb');
 
 function create (port) {
+    var mb;
 
     function mbServer (command, args) {
         var deferred = Q.defer(),
             calledDone = false,
-            mbArgs = [command, '--port', port, '--pidfile', 'test.pid'].concat(args || []),
-            mb;
+            mbArgs = [command, '--port', port, '--pidfile', 'test.pid'].concat(args || []);
+
+        if (command == 'stop') {
+            // Need to troubleshoot more; spawning 'mb stop' on Windows failed
+            mb.kill();
+            return Q(true);
+        }
 
         if (isWindows) {
             mbArgs.unshift(mbPath);
@@ -23,8 +29,9 @@ function create (port) {
         }
 
         ['stdout', 'stderr'].forEach(function (stream) {
-            mb[stream].on('data', function () {
-                if (!calledDone) {
+            mb[stream].on('data', function (data) {
+                var finished = command === 'stop' || data.toString('utf8').indexOf('now taking orders') >= 0;
+                if (finished && !calledDone) {
                     calledDone = true;
                     deferred.resolve();
                 }
