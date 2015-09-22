c027ec9d8eb642a408792c0ec3bbd7dc3b9d6b17
Brandon Byars <brandon.byars@gmail.com>
2015-09-21 19:22:54 -0500

    Trying to get test version set for npm publish

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -34,7 +34,6 @@ module.exports = function (grunt) {
             return;
         }
 
-        console.log('*******buildNumber: ' + buildNumber);
         run('scripts/deploy/deployNpm', [publish, buildNumber]).done(this.async(), failTask('deploy:npm'));
     });
 };
