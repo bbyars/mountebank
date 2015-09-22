cdad00519e210a3eb6af585eff64a63d097d3b17
Brandon Byars <brandon.byars@gmail.com>
2015-09-21 20:52:14 -0500

    Only running W3C validation tests in one node of the matrix They're slow and occasionally fragile, and there's no value in running them multiple times

--- functionalTest/html/validatorTest.js
+++ functionalTest/html/validatorTest.js
@@ -7,8 +7,10 @@ var assert = require('assert'),
     fs = require('fs'),
     timeout = 10000;
 
-// These require network access
-if (process.env.MB_AIRPLANE_MODE !== 'true') {
+// MB_AIRPLANE_MODE because these require network access
+// MB_SKIP_W3C_TESTS because these are slow, occasionally fragile, and there's
+// no value running them with every node in the build matrix
+if (process.env.MB_AIRPLANE_MODE !== 'true' && process.env.MB_SKIP_W3C_TESTS !== 'true') {
     describe('html validation', function () {
         this.timeout(timeout);
 
@@ -38,46 +40,46 @@ if (process.env.MB_AIRPLANE_MODE !== 'true') {
             '/releases',
             '/releases/v1.3.0' // save time by only checking latest releases, others should be immutable
         ].forEach(function (endpoint) {
-                it(endpoint + ' should have no html errors', function (done) {
-                    var spec = {
-                        port: api.port,
-                        method: 'GET',
-                        path: endpoint,
-                        headers: {accept: 'text/html'}
-                    };
+            it(endpoint + ' should have no html errors', function (done) {
+                var spec = {
+                    port: api.port,
+                    method: 'GET',
+                    path: endpoint,
+                    headers: { accept: 'text/html' }
+                };
 
-                    httpClient.responseFor(spec).then(function (response) {
-                        // ignore errors for webkit attributes on search box
-                        // use unique filename each time because otherwise a timed out test
-                        // causes the next test(s) to fail
-                        var body = response.body.replace("results='5' autosave='mb' ", ''),
-                            filename = endpoint.replace(/\//g, '') + '-validation-test.html';
-                        fs.writeFileSync(filename, body);
+                httpClient.responseFor(spec).then(function (response) {
+                    // ignore errors for webkit attributes on search box
+                    // use unique filename each time because otherwise a timed out test
+                    // causes the next test(s) to fail
+                    var body = response.body.replace("results='5' autosave='mb' ", ''),
+                        filename = endpoint.replace(/\//g, '') + '-validation-test.html';
+                    fs.writeFileSync(filename, body);
 
-                        w3cjs.validate({
-                            file: filename,
-                            callback: function (response) {
-                                fs.unlinkSync(filename);
+                    w3cjs.validate({
+                        file: filename,
+                        callback: function (response) {
+                            fs.unlinkSync(filename);
 
-                                if (response.messages) {
-                                    var errors = response.messages.filter(function (message) {
-                                        return message.type === 'error';
-                                    }).map(function (message) {
-                                        return {
-                                            line: message.lastLine,
-                                            message: message.message
-                                        };
-                                    });
-                                    assert.strictEqual(0, errors.length, JSON.stringify(errors, null, 2));
-                                }
-                                else {
-                                    console.warn('HTML validation skipped for ' + endpoint);
-                                }
-                                done();
+                            if (response.messages) {
+                                var errors = response.messages.filter(function (message) {
+                                    return message.type === 'error';
+                                }).map(function (message) {
+                                    return {
+                                        line: message.lastLine,
+                                        message: message.message
+                                    };
+                                });
+                                assert.strictEqual(0, errors.length, JSON.stringify(errors, null, 2));
                             }
-                        });
+                            else {
+                                console.warn('HTML validation skipped for ' + endpoint);
+                            }
+                            done();
+                        }
                     });
                 });
             });
+        });
     });
 }
