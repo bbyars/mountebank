a26b104f79a253b252369da7e04f490da1b3742a
Brandon Byars <brandon.byars@gmail.com>
2015-09-25 20:09:03 -0500

    Triggering Appveyor from Travis to keep same version info

---
+++ tasks/ci/appveyor.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var Q = require('q'),
+    https = require('https'),
+    apiToken = process.env.APPVEYOR_API_TOKEN;
+
+function responseFor (options) {
+    if (!apiToken) {
+        throw 'APPVEYOR_API_TOKEN environment variable must be set';
+    }
+
+    var deferred = Q.defer();
+
+    options.hostname = 'ci.appveyor.com';
+    options.headers = {
+        Authorization: 'Bearer dwr0481y4kxcgyerdxiy'
+    };
+
+    var request = https.request(options, function (response) {
+        var packets = [];
+
+        response.on('data', function (data) {
+            packets.push(data);
+        });
+
+        response.on('end', function () {
+            var contentType = response.headers['content-type'] || '';
+
+            response.body = Buffer.concat(packets).toString('utf8');
+            if (contentType.indexOf('application/json') === 0) {
+                response.body = JSON.parse(response.body);
+            }
+            deferred.resolve(response);
+        });
+    });
+
+    request.on('error', deferred.reject);
+
+    // Appveyor APIs appear to be deeply sensitive in ways that are deeply hard
+    // to comprehend to both the capitalization of some headers (e.g. Authorization)
+    // and maybe the ordering of some headers.  This block below fails if put in
+    // the options object passed into https.request above.
+    if (options.body) {
+        options.body = JSON.stringify(options.body);
+        request.setHeader('Content-Type', 'application/json');
+        request.setHeader('Content-Length', options.body.length);
+    }
+
+    if (options.body) {
+        request.write(options.body);
+    }
+    request.end();
+    return deferred.promise;
+}
+
+function triggerBuild (commitId, version) {
+    // From what I can tell, POST /api/builds accepts a full SHA for the commitId IF
+    // no environmentVariables are passed, but only an 8 digit SHA with environmentVariables
+    return responseFor({
+        method: 'POST',
+        path: '/api/builds',
+        body: {
+            accountName: 'bbyars',
+            projectSlug: 'mountebank',
+            branch: 'master',
+            commitId: commitId.substring(0,8),
+            environmentVariables: {
+                MB_VERSION: version
+            }
+        }
+    }).then(function (response) {
+        if (response.statusCode !== 200) {
+            console.error('Status code of POST /api/builds: ' + response.statusCode);
+            throw response.body;
+        }
+
+        return response.body;
+    });
+}
+
+module.exports = {
+    triggerBuild: triggerBuild
+};
---
+++ tasks/pipeline.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var Q = require('q'),
+    exec = require('child_process').exec,
+    thisPackage = require('../package.json'),
+    version = process.env.MB_VERSION || thisPackage.version,
+    appveyor = require('./ci/appveyor');
+
+function getCurrentCommitId () {
+    var deferred = Q.defer();
+    exec('git rev-parse HEAD', function (error, stdout) {
+        if (error) {
+            deferred.reject(error);
+        }
+        else {
+            deferred.resolve(stdout.trim());
+        }
+    });
+    return deferred.promise;
+}
+
+module.exports = function (grunt) {
+    grunt.registerTask('trigger:appveyor', 'Trigger Appveyor build for same commit', function () {
+        var done = this.async();
+
+        return getCurrentCommitId().then(function (commitId) {
+            return appveyor.triggerBuild(commitId, version);
+        }).then(function (result) {
+            console.log('Appveyor build successfully triggered for ' + version + ' => ' + result.version);
+            done();
+        }, function (error) {
+            grunt.warn(error);
+        });
+    });
+};
