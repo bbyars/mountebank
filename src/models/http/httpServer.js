36881dba1aef5681b1f9c5e1cf7b00ab50bad904
Brandon Byars <brandon.byars@gmail.com>
2015-10-23 13:02:35 -0500

    Monkey patching rawHeaders for node 0.10 to support issue #75

--- src/models/http/httpServer.js
+++ src/models/http/httpServer.js
@@ -10,6 +10,27 @@ var baseHttpServer = require('./baseHttpServer'),
         };
     };
 
+function patchRawHeaders () {
+    var requestPrototype = http.IncomingMessage.prototype,
+        _addHeaderLine = requestPrototype._addHeaderLine;
+
+    // Patch ServerRequest to save unmodified copy of headers so we get original case
+    // (see https://github.com/bbyars/mountebank/issues/75)
+    // This is only needed for node 0.10 compatibility as we get rawHeaders with 0.12+
+    // Adapted rom http://grokbase.com/t/gg/nodejs/125ynyxa6c/need-raw-http-headers
+    requestPrototype._addHeaderLine = function (field, value) {
+        this.rawHeaders = this.rawHeaders || [];
+        this.rawHeaders.push(field);
+        this.rawHeaders.push(value);
+        _addHeaderLine.call(this, field, value);
+    };
+}
+
 module.exports = {
-    initialize: baseHttpServer.setup('http', createBaseServer).initialize
+    initialize: function (allowInjection, mock, debug) {
+        if (process.version.indexOf('v0.10') === 0) {
+            patchRawHeaders();
+        }
+        baseHttpServer.setup('http', createBaseServer).initialize(allowInjection, mock, debug);
+    }
 };
