c411a2ed46958b44eaa48305834220208d9fc591
Brandon Byars <brandon.byars@gmail.com>
2015-10-26 18:40:16 -0500

    Added ability to page logs for autoscrolling /logs page

---
+++ test/controllers/logsControllerTest.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var Controller = require('../../src/controllers/logsController'),
+    assert = require('assert'),
+    fs = require('fs'),
+    FakeResponse = require('../fakes/fakeResponse');
+
+describe('logsController', function () {
+    describe('#get', function () {
+        after(function () {
+            fs.unlinkSync('logsControllerTest.log');
+        });
+
+        it('should return full contents of logfile as JSON array by default', function () {
+            var response = FakeResponse.create(),
+                controller = Controller.create('logsControllerTest.log');
+
+            fs.writeFileSync('logsControllerTest.log', '{"key": "first"}\n{"key": "second"}\n');
+            controller.get({ url: '/logs' }, response);
+
+            assert.deepEqual(response.body, {
+                logs: [
+                    { key: 'first' },
+                    { key: 'second' }
+                ]
+            });
+        });
+
+        it('should return entries starting with startIndex', function () {
+            var response = FakeResponse.create(),
+                controller = Controller.create('logsControllerTest.log');
+
+            fs.writeFileSync('logsControllerTest.log', '{"key": "first"}\n{"key": "second"}\n{"key": "third"}');
+            controller.get({ url: '/logs?startIndex=1' }, response);
+
+            assert.deepEqual(response.body, {
+                logs: [
+                    { key: 'second' },
+                    { key: 'third' }
+                ]
+            });
+        });
+
+        it('should return entries starting with startIndex and ending with endIndex', function () {
+            var response = FakeResponse.create(),
+                controller = Controller.create('logsControllerTest.log');
+
+            fs.writeFileSync('logsControllerTest.log', '{"key": "first"}\n{"key": "second"}\n{"key": "third"}');
+            controller.get({ url: '/logs?startIndex=0&endIndex=1' }, response);
+
+            assert.deepEqual(response.body, {
+                logs: [
+                    { key: 'first' },
+                    { key: 'second' }
+                ]
+            });
+        });
+    });
+});
