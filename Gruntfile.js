765a18bceee182402a106beae9073062d54f9e7c
Brandon Byars <brandon.byars@gmail.com>
2015-11-14 19:09:43 -0600

    Removing dead code

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-jsdoc');
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
         mochaTest: {
             unit: {
                 options: {
