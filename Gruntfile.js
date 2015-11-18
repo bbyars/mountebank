6068ebe4b69a1ee1ab81f7375f144eb58c5b6dfc
Brandon Byars <brandon.byars@gmail.com>
2015-11-18 10:03:34 -0600

    Removing troubleshooting code fixing integration with grunt-mountebank https://github.com/bbyars/mountebank/issues/114

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,7 @@ var port = process.env.MB_PORT || 2525;
 
 module.exports = function (grunt) {
 
-    if (process.env.TRAVIS !== 'true') {
-        // Unclear why, but this errors some of the Travis builds
-        require('time-grunt')(grunt);
-    }
+    require('time-grunt')(grunt);
 
     grunt.loadTasks('tasks');
     grunt.loadNpmTasks('grunt-mocha-test');
