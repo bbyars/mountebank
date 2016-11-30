7ae539db9f5529508ce5c5e195b9e13f973f7ce0
Brandon Byars <brandon.byars@gmail.com>
2016-11-30 10:12:48 -0500

    Fixing pin icon interaction bug with google analytics https://github.com/bbyars/mountebank/issues/140

--- src/public/scripts/trackOutgoingLinks.js
+++ src/public/scripts/trackOutgoingLinks.js
@@ -11,41 +11,49 @@ $(document).ready(function () {
     }
 
     $('a').on('click', function () {
-        var element = $(this),
-            track = false,
-            href = element.attr('href'),
-            domains = document.domain.split('.').reverse(),
-            isThisDomain = href.match(domains[1] + '.' + domains[0]),
-            events = [];
+        try {
+            var element = $(this),
+                track = false,
+                href = element.attr('href'),
+                domains = document.domain.split('.').reverse(),
+                isThisDomain = href.match(domains[1] + '.' + domains[0]),
+                events = [];
 
-        events.value = 0;
+            events.value = 0;
 
-        if (href.match(filetypes)) {
-            var extension = (/[.]/.exec(href)) ? /[^.]+$/.exec(href) : undefined;
-            events.category = 'download';
-            events.action = 'click-' + extension[0];
-            events.label = href.replace(/ /g, '-');
-            events.nonInteraction = false;
-            events.location = baseHref + href;
-            track = true;
-        }
-        else if (href.match(/^https?\:/i) && !isThisDomain) {
-            events.category = 'external';
-            events.action = 'click';
-            events.label = href.replace(/^https?\:\/\//i, '');
-            events.nonInteraction = true;
-            events.location = href;
-            track = true;
-        }
-
-        if (track) {
-            /* global _gaq */
-            _gaq.push(['_trackEvent', events.category, events.action, events.label, events.value, events.nonInteraction]);
-            if (element.attr('target') === undefined || element.attr('target').toLowerCase() !== '_blank') {
-                setTimeout(function () { location.href = events.location; }, 400);
-                return false;
+            if (href.match(filetypes)) {
+                var extension = (/[.]/.exec(href)) ? /[^.]+$/.exec(href) : undefined;
+                events.category = 'download';
+                events.action = 'click-' + extension[0];
+                events.label = href.replace(/ /g, '-');
+                events.nonInteraction = false;
+                events.location = baseHref + href;
+                track = true;
             }
+            else if (href.match(/^https?\:/i) && !isThisDomain) {
+                events.category = 'external';
+                events.action = 'click';
+                events.label = href.replace(/^https?\:\/\//i, '');
+                events.nonInteraction = true;
+                events.location = href;
+                track = true;
+            }
+
+            if (track) {
+                /* global _gaq */
+                _gaq.push(['_trackEvent', events.category, events.action, events.label, events.value, events.nonInteraction]);
+                if (element.attr('target') === undefined || element.attr('target').toLowerCase() !== '_blank') {
+                    setTimeout(function () {
+                        location.href = events.location;
+                    }, 400);
+                    return false;
+                }
+            }
+            return true;
         }
-        return true;
+        catch (err) {
+            console.log(err);
+            return true;
+        }
     });
 });
