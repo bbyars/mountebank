e26d24b6ab2d99c9ed8c97e9f91a110a769454fc
Brandon Byars <brandon.byars@gmail.com>
2017-07-03 12:47:55 -0500

    Fixing grunt cache call to wait until finished

--- tasks/cache.js
+++ tasks/cache.js
@@ -3,9 +3,8 @@
 var run = require('./run').run;
 
 module.exports = function (grunt) {
-
     grunt.registerTask('cache', 'Save npm cache', function () {
-        run('scripts/cache').done(this.async, function (exitCode) {
+        run('scripts/cache', []).done(this.async(), function (exitCode) {
             grunt.warn('cache failed', exitCode);
         });
     });
