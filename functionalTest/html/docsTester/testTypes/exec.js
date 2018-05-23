aa5b17a7e33d2c646abd611c9a9d9cd91f8a78ff
Brandon Byars <brandon.byars@gmail.com>
2018-05-23 10:48:10 -0500

    I believe the netcat problem requires the -q parameter on Circle's image https://github.com/bbyars/mountebank/issues/308

--- functionalTest/html/docsTester/testTypes/exec.js
+++ functionalTest/html/docsTester/testTypes/exec.js
@@ -19,13 +19,24 @@ function execute (command) {
     return deferred.promise;
 }
 
+function ensureOSCompatibility (command) {
+    // On CircleCI image, netcat requires the -q1 parameter to end after receiving stdin
+    if (require('os').platform() === 'linux') {
+        return command.replace('| nc ', '| nc -q1 ');
+    }
+    else {
+        // Mac netcat doesn't support the -q parameter
+        return command;
+    }
+}
+
 function runStep (step) {
     var deferred = Q.defer(),
         filename = 'test-' + nextTestId;
 
 console.log('========================' + new Date() + '========================');
 console.log('writing ' + filename + ':\n' + step.requestText);
-    fs.writeFileSync(filename, step.requestText, { mode: 484 /* 0744 */});
+    fs.writeFileSync(filename, ensureOSCompatibility(step.requestText), { mode: 484 /* 0744 */});
     nextTestId += 1;
 
 console.log('========================' + new Date() + '========================');
