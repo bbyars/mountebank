53d7b3dcb9a35f22478e9c5ea52ca855e44bb64f
Brandon Byars <brandon.byars@gmail.com>
2018-06-17 21:19:11 -0500

    Experimenting removing the -q netcat parameter
    It fails on the centos version of netcat
    https://github.com/bbyars/mountebank/issues/308

--- functionalTest/html/docsTester/testTypes/exec.js
+++ functionalTest/html/docsTester/testTypes/exec.js
@@ -19,26 +19,26 @@ function execute (command) {
     return deferred.promise;
 }
 
-function ensureOSCompatibility (command) {
-    // On CircleCI image, netcat requires the -q1 parameter to end after receiving stdin
-    // This feels ugly to do the replacement here, but I prefer it over doing the replacement
-    // directly in the views because I want people to be able to copy the command from the
-    // public site and run on their machines, and the variant without the -q is the most portable
-    // even though the public site is hosted on linux
-    if (require('os').platform() === 'linux') {
-        return command.replace('| nc ', '| nc -q1 ');
-    }
-    else {
-        // Mac netcat doesn't support the -q parameter
-        return command;
-    }
-}
+// function ensureOSCompatibility (command) {
+//    // On CircleCI image, netcat requires the -q1 parameter to end after receiving stdin
+//    // This feels ugly to do the replacement here, but I prefer it over doing the replacement
+//    // directly in the views because I want people to be able to copy the command from the
+//    // public site and run on their machines, and the variant without the -q is the most portable
+//    // even though the public site is hosted on linux
+//    if (require('os').platform() === 'linux') {
+//        return command.replace('| nc ', '| nc -q1 ');
+//    }
+//    else {
+//        // Mac netcat doesn't support the -q parameter
+//        return command;
+//    }
+// }
 
 function runStep (step) {
     var deferred = Q.defer(),
         filename = 'test-' + nextTestId;
 
-    fs.writeFileSync(filename, ensureOSCompatibility(step.requestText), { mode: 484 /* 0744 */});
+    fs.writeFileSync(filename, step.requestText, { mode: 484 /* 0744 */});
     nextTestId += 1;
 
     execute('sh ./' + filename).done(function (stdout) {
