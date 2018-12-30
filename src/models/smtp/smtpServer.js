e756636b5c3466455193be30b82c656c9cf03082
Brandon Byars <brandon.byars@gmail.com>
2018-12-30 09:35:38 -0600

    Unwound smtp from the nefarious grip of AbstractServer
    Prepping new interface conversion
    https://github.com/bbyars/mountebank/issues/174

--- src/models/smtp/smtpServer.js
+++ src/models/smtp/smtpServer.js
@@ -5,56 +5,138 @@
  * @module
  */
 
-function createServer () {
-    function createSMTPServer (server) {
-        const SMTPServer = require('smtp-server').SMTPServer;
-        return new SMTPServer({
-            disableReverseLookup: true,
-            authOptional: true,
-            onConnect (session, callback) {
-                server.emit('connection', session);
-                return callback();
-            },
-            onData (stream, session, callback) {
-                server.emit('request', session, { session: session, source: stream, callback: callback });
+function implement (recordRequests, debug, baseLogger) {
+
+    function create (options) {
+        options.recordRequests = options.recordRequests || recordRequests;
+        options.debug = debug;
+
+        function scopeFor (port) {
+            const util = require('util');
+            let scope = util.format('%s:%s', 'smtp', port);
+
+            if (options.name) {
+                scope += ` ${options.name}`;
             }
+            return scope;
+        }
+
+        let numRequests = 0;
+        const Q = require('q'),
+            deferred = Q.defer(),
+            requests = [],
+            logger = require('../../util/scopedLogger').create(baseLogger, scopeFor(options.port)),
+            connections = {};
+
+        function createSMTPServer () {
+            const SMTPServer = require('smtp-server').SMTPServer;
+            return new SMTPServer({
+                disableReverseLookup: true,
+                authOptional: true,
+                onConnect (socket, callback) {
+                    const helpers = require('../../util/helpers'),
+                        name = helpers.socketName(socket);
+
+                    logger.debug('%s ESTABLISHED', name);
+
+                    if (socket.on) {
+                        connections[name] = socket;
+
+                        socket.on('error', error => {
+                            logger.error('%s transmission error X=> %s', name, JSON.stringify(error));
+                        });
+
+                        socket.on('end', () => {
+                            logger.debug('%s LAST-ACK', name);
+                        });
+
+                        socket.on('close', () => {
+                            logger.debug('%s CLOSED', name);
+                            delete connections[name];
+                        });
+                    }
+                    return callback();
+                },
+                onData (stream, socket, callback) {
+                    const request = { session: socket, source: stream, callback: callback };
+                    const domain = require('domain').create(),
+                        helpers = require('../../util/helpers'),
+                        clientName = helpers.socketName(socket),
+                        errorHandler = error => {
+                            const exceptions = require('../../util/errors');
+                            logger.error('%s X=> %s', clientName, JSON.stringify(exceptions.details(error)));
+                        };
+
+                    logger.info(`${clientName} => Envelope from: ${request.from} to: ${JSON.stringify(request.to)}`);
+
+                    domain.on('error', errorHandler);
+                    domain.run(() => {
+                        require('./smtpRequest').createFrom(request).then(simpleRequest => {
+                            logger.debug('%s => %s', clientName, JSON.stringify(simpleRequest));
+                            numRequests += 1;
+                            if (options.recordRequests) {
+                                const recordedRequest = helpers.clone(simpleRequest);
+                                recordedRequest.timestamp = new Date().toJSON();
+                                requests.push(recordedRequest);
+                            }
+                            return Q(request.callback());
+                        }).done(response => {
+                            if (response) {
+                                logger.debug('%s <= %s', clientName, JSON.stringify(response));
+                            }
+                        }, errorHandler);
+                    });
+
+                }
+            });
+        }
+
+        const server = createSMTPServer();
+
+        server.listen(options.port || 0, () => {
+            const actualPort = server.server.address().port;
+            const metadata = {};
+            if (options.name) {
+                metadata.name = options.name;
+            }
+
+            if (options.port !== actualPort) {
+                logger.changeScope(scopeFor(actualPort));
+            }
+
+            logger.info('Open for business...');
+
+            /**
+             * This is the interface for all protocols
+             */
+            deferred.resolve({
+                numberOfRequests: () => numRequests,
+                requests,
+                addStub: () => {},
+                stubs: () => [],
+                metadata,
+                port: actualPort,
+                close: () => {
+                    const closeDeferred = Q.defer();
+                    server.close(() => {
+                        logger.info('Ciao for now');
+                        closeDeferred.resolve();
+                    });
+                    Object.keys(connections).forEach(socket => {
+                        connections[socket].destroy();
+                    });
+                    return closeDeferred.promise;
+                },
+                resetProxies: server.resetProxies
+            });
         });
+
+        return deferred.promise;
     }
 
-    const combinators = require('../../util/combinators'),
-        inherit = require('../../util/inherit'),
-        result = inherit.from(require('events').EventEmitter, {
-            errorHandler: require('../../util/combinators').noop,
-            formatRequestShort: request => {
-                const util = require('util');
-                return util.format('Envelope from: %s to: %s', request.from, JSON.stringify(request.to));
-            },
-            formatRequest: combinators.identity,
-            formatResponse: combinators.noop,
-            respond: (smtpRequest, originalRequest) => { originalRequest.callback(); },
-            metadata: combinators.constant({}),
-            addStub: combinators.noop,
-            state: {},
-            stubs: () => []
-        }),
-        server = createSMTPServer(result);
-
-    result.close = callback => {
-        server.close(combinators.noop);
-        callback();
+    return {
+        create
     };
-
-    result.listen = port => {
-        const Q = require('q'),
-            deferred = Q.defer();
-
-        server.listen(port, () => {
-            deferred.resolve(server.server.address().port);
-        });
-        return deferred.promise;
-    };
-
-    return result;
 }
 
 /**
@@ -65,15 +147,9 @@ function createServer () {
  * @returns {Object}
  */
 function initialize (logger, recordRequests, debug) {
-    const implementation = {
-        protocolName: 'smtp',
-        createServer: createServer,
-        Request: require('./smtpRequest')
-    };
-
     return {
-        name: implementation.protocolName,
-        create: require('../abstractServer').implement(implementation, recordRequests, debug, logger).create,
+        name: 'smtp',
+        create: implement(recordRequests, debug, logger).create,
         testRequest: {
             from: 'test@test.com',
             to: ['test@test.com'],
