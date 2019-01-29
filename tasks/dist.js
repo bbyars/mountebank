38a38aad24a10863f196b634cb3e09dc0ef8ab6d
Brandon Byars <brandon.byars@gmail.com>
2019-01-29 06:41:09 -0600

    Adding protocols.json to distribution
    https://github.com/bbyars/mountebank/issues/174

--- tasks/dist.js
+++ tasks/dist.js
@@ -9,12 +9,12 @@ const fs = require('fs-extra'),
 module.exports = function (grunt) {
 
     function failTask (task) {
-        return function (exitCode) {
+        return exitCode => {
             grunt.warn(task + ' failed', exitCode);
         };
     }
 
-    grunt.registerTask('dist', 'Create trimmed down distribution directory', function () {
+    grunt.registerTask('dist', 'Create trimmed down distribution directory', () => {
         const done = this.async(),
             newPackage = JSON.parse(JSON.stringify(require('../package.json'))),
             failed = failTask('dist');
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
         rimraf.sync('dist');
         fs.mkdirSync('dist');
         fs.mkdirSync('dist/mountebank');
-        ['bin', 'src', 'package.json', 'package-lock.json', 'releases.json', 'README.md', 'LICENSE'].forEach(function (source) {
+        ['bin', 'src', 'package.json', 'package-lock.json', 'releases.json', 'protocols.json', 'README.md', 'LICENSE'].forEach(source => {
             fs.copySync(source, 'dist/mountebank/' + source);
         });
         rimraf.sync('dist/mountebank/src/public/images/sources');
@@ -31,14 +31,14 @@ module.exports = function (grunt) {
         delete newPackage.devDependencies;
         fs.writeFileSync('dist/mountebank/package.json', JSON.stringify(newPackage, null, 2));
 
-        run('npm', ['install', '--production'], { cwd: 'dist/mountebank' }).done(function () {
+        run('npm', ['install', '--production'], { cwd: 'dist/mountebank' }).done(() => {
             // Switch tests to use the mb from the dist directory to test what actually gets published
             process.env.MB_EXECUTABLE = 'dist/mountebank/bin/mb';
             done();
         }, failed);
     });
 
-    grunt.registerTask('version', 'Set the version number', function () {
+    grunt.registerTask('version', 'Set the version number', () => {
         const newPackage = require('../dist/mountebank/package.json');
 
         newPackage.version = version;
@@ -46,22 +46,22 @@ module.exports = function (grunt) {
         fs.writeFileSync('./dist/mountebank/package.json', JSON.stringify(newPackage, null, 2) + '\n');
     });
 
-    grunt.registerTask('dist:tarball', 'Create OS-specific tarballs', function (arch) {
+    grunt.registerTask('dist:tarball', 'Create OS-specific tarballs', arch => {
         run('scripts/dist/createSelfContainedTarball', [os.platform(), arch || os.arch(), version]).done(
             this.async(), failTask('dist:tarball'));
     });
 
-    grunt.registerTask('dist:zip', 'Create OS-specific zips', function (arch) {
+    grunt.registerTask('dist:zip', 'Create OS-specific zips', arch => {
         run('scripts/dist/createWindowsZip', [arch, version]).done(this.async(), failTask('dist:zip'));
     });
 
-    grunt.registerTask('dist:npm', 'Create npm tarball', function () {
+    grunt.registerTask('dist:npm', 'Create npm tarball', () => {
         const filename = 'mountebank-v' + version + '-npm.tar.gz';
 
         run('tar', ['czf', filename, 'mountebank'], { cwd: 'dist' }).done(this.async(), failTask('dist:npm'));
     });
 
-    grunt.registerTask('dist:package', 'Create OS-specific package', function (type) {
+    grunt.registerTask('dist:package', 'Create OS-specific package', type => {
         run('scripts/dist/createPackage', [os.platform(), type, version]).done(this.async(), failTask('dist:package'));
     });
 };
