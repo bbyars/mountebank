769f87f7dbc24855c5afb96ad07b4037468c0808
Brandon Byars <brandon.byars@gmail.com>
2019-05-20 20:18:15 -0500

    Troubleshooting ECONNRESET errors on Windows build

--- functionalTest/html/feedTest.js
+++ functionalTest/html/feedTest.js
@@ -21,25 +21,15 @@ function getNextLink (body) {
 
 describe('the feed', function () {
     promiseIt('should default to page 1 with 10 entries', function () {
-        console.log('******************');
-        console.log('    1.....');
         return httpClient.get('/feed', api.port).then(response => {
-            console.log('    2.....');
             assert.strictEqual(response.statusCode, 200);
             assert.strictEqual(response.headers['content-type'], 'application/atom+xml; charset=utf-8');
-            console.log(response.body);
             assert.strictEqual(entryCount(response.body), 10);
 
-            console.log('    3.....');
             return httpClient.get(getNextLink(response.body), api.port);
         }).then(response => {
-            console.log('    4.....');
             assert.strictEqual(response.statusCode, 200);
             assert.ok(entryCount(response.body) > 0, 'No entries');
-            console.log('    5.....');
-        }, err => {
-            console.log('FEED ERROR:');
-            console.log(err);
         });
     });
 });
