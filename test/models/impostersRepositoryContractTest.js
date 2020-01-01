3d2edac1baac746a6573ddf84fba66a4d948029f
Brandon Byars <brandon.byars@gmail.com>
2019-12-31 23:06:48 -0500

    Finished migrating memory repo tests to repo contract tests
    https://github.com/bbyars/mountebank/issues/279

--- test/models/impostersRepositoryContractTest.js
+++ test/models/impostersRepositoryContractTest.js
@@ -353,6 +353,96 @@ types.forEach(function (type) {
                         });
                 });
             });
+
+            describe('#overwriteAll', function () {
+                promiseIt('should overwrite entire list', function () {
+                    const stubs = repo.stubsFor(1),
+                        firstStub = { responses: [{ is: 'first' }, { is: 'second' }] },
+                        secondStub = { responses: [{ is: 'third' }, { is: 'fourth' }] },
+                        thirdStub = { responses: [{ is: 'fifth' }, { is: 'sixth' }] };
+
+                    return stubs.add(firstStub)
+                        .then(() => stubs.add(secondStub))
+                        .then(() => stubs.overwriteAll([thirdStub]))
+                        .then(() => {
+                            return stubs.toJSON().then(all => {
+                                const responses = all.map(stub => stub.responses);
+
+                                assert.deepEqual(responses, [
+                                    [{ is: 'fifth' }, { is: 'sixth' }]
+                                ]);
+                            });
+                        });
+                });
+            });
+
+            describe('#overwriteAtIndex', function () {
+                promiseIt('should overwrite single stub', function () {
+                    const stubs = repo.stubsFor(1),
+                        firstStub = { responses: [{ is: 'first' }, { is: 'second' }] },
+                        secondStub = { responses: [{ is: 'third' }, { is: 'fourth' }] },
+                        thirdStub = { responses: [{ is: 'fifth' }, { is: 'sixth' }] };
+
+                    return stubs.add(firstStub)
+                        .then(() => stubs.add(secondStub))
+                        .then(() => stubs.overwriteAtIndex(thirdStub, 1))
+                        .then(() => stubs.toJSON())
+                        .then(all => {
+                            const responses = all.map(stub => stub.responses);
+
+                            assert.deepEqual(responses, [
+                                [{ is: 'first' }, { is: 'second' }],
+                                [{ is: 'fifth' }, { is: 'sixth' }]
+                            ]);
+                        });
+                });
+            });
+
+            describe('#deleteAtIndex', function () {
+                promiseIt('should overwrite single stub', function () {
+                    const stubs = repo.stubsFor(1),
+                        firstStub = { responses: [{ is: 'first' }, { is: 'second' }] },
+                        secondStub = { responses: [{ is: 'third' }, { is: 'fourth' }] },
+                        thirdStub = { responses: [{ is: 'fifth' }, { is: 'sixth' }] };
+
+                    return stubs.add(firstStub)
+                        .then(() => stubs.add(secondStub))
+                        .then(() => stubs.add(thirdStub))
+                        .then(() => stubs.deleteAtIndex(0))
+                        .then(() => stubs.toJSON())
+                        .then(all => {
+                            const responses = all.map(stub => stub.responses);
+
+                            assert.deepEqual(responses, [
+                                [{ is: 'third' }, { is: 'fourth' }],
+                                [{ is: 'fifth' }, { is: 'sixth' }]
+                            ]);
+                        });
+                });
+            });
+
+            describe('#insertAtIndex', function () {
+                promiseIt('should add single stub at given index', function () {
+                    const stubs = repo.stubsFor(1),
+                        firstStub = { responses: [{ is: 'first' }, { is: 'second' }] },
+                        secondStub = { responses: [{ is: 'third' }, { is: 'fourth' }] },
+                        insertedStub = { responses: [{ is: 'fifth' }, { is: 'sixth' }] };
+
+                    return stubs.add(firstStub)
+                        .then(() => stubs.add(secondStub))
+                        .then(() => stubs.insertAtIndex(insertedStub, 0))
+                        .then(() => stubs.toJSON())
+                        .then(all => {
+                            const responses = all.map(stub => stub.responses);
+
+                            assert.deepEqual(responses, [
+                                [{ is: 'fifth' }, { is: 'sixth' }],
+                                [{ is: 'first' }, { is: 'second' }],
+                                [{ is: 'third' }, { is: 'fourth' }]
+                            ]);
+                        });
+                });
+            });
         });
     });
 });
