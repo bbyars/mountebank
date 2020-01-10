852b4aea653967d62911767b58c8f917dcbc7d9d
Brandon Byars <brandon.byars@gmail.com>
2020-01-09 19:37:16 -0600

    Optimized imposter JSON generation to avoid unnecessary file lookups
    https://github.com/bbyars/mountebank/issues/279

--- src/models/imposterPrinter.js
+++ src/models/imposterPrinter.js
@@ -1,92 +1,115 @@
 'use strict';
 
 function create (creationRequest, server, loadRequests) {
-    function addDetailsTo (result, baseURL) {
-        if (creationRequest.name) {
-            result.name = creationRequest.name;
+    const baseURL = `/imposters/${server.port}`;
+
+    function createHeader (numberOfRequests, options) {
+        const result = {
+            protocol: creationRequest.protocol,
+            port: server.port
+        };
+
+        if (!options.replayable) {
+            result.numberOfRequests = numberOfRequests;
         }
-        result.recordRequests = Boolean(creationRequest.recordRequests);
-
-        Object.keys(server.metadata).forEach(key => {
-            result[key] = server.metadata[key];
-        });
-
-        return loadRequests().then(requests => {
-            result.requests = requests;
-            return server.stubs.toJSON({ debug: true });
-        }).then(all => {
-            result.stubs = all;
-            for (let i = 0; i < result.stubs.length; i += 1) {
-                result.stubs[i]._links = {
-                    self: { href: `${baseURL}/stubs/${i}` }
-                };
+        if (!options.list) {
+            if (creationRequest.name) {
+                result.name = creationRequest.name;
             }
-            return result;
+            result.recordRequests = Boolean(creationRequest.recordRequests);
+
+            Object.keys(server.metadata).forEach(key => {
+                result[key] = server.metadata[key];
+            });
+        }
+
+        return result;
+    }
+
+    function addStubsTo (imposter, options) {
+        const newOptions = {};
+        if (!options.replayable) {
+            newOptions.debug = true;
+        }
+
+        return server.stubs.toJSON(newOptions).then(all => {
+            imposter.stubs = all;
+            return imposter;
         });
     }
 
-    function removeNonEssentialInformationFrom (result) {
+    function removeNonEssentialInformationFrom (imposter) {
         const helpers = require('../util/helpers');
 
-        result.stubs.forEach(stub => {
-            /* eslint-disable no-underscore-dangle */
-            if (stub.matches) {
-                delete stub.matches;
-            }
+        imposter.stubs.forEach(stub => {
             stub.responses.forEach(response => {
-                if (helpers.defined(response.is) && helpers.defined(response.is._proxyResponseTime)) {
-                    delete response.is._proxyResponseTime;
+                if (helpers.defined(response.is)) {
+                    delete response.is._proxyResponseTime; // eslint-disable-line no-underscore-dangle
                 }
             });
-            delete stub._links;
         });
-        delete result.numberOfRequests;
-        delete result.requests;
-        delete result._links;
     }
 
-    function removeProxiesFrom (result) {
-        result.stubs.forEach(stub => {
+    function addRequestsTo (imposter) {
+        return loadRequests().then(requests => {
+            imposter.requests = requests;
+            return imposter;
+        });
+    }
+
+    function removeProxiesFrom (imposter) {
+        imposter.stubs.forEach(stub => {
             // eslint-disable-next-line no-prototype-builtins
             stub.responses = stub.responses.filter(response => !response.hasOwnProperty('proxy'));
         });
-        result.stubs = result.stubs.filter(stub => stub.responses.length > 0);
+        imposter.stubs = imposter.stubs.filter(stub => stub.responses.length > 0);
     }
 
-    function removeFunctions (obj) {
-        return JSON.parse(JSON.stringify(obj));
+    function addLinksTo (imposter) {
+        imposter._links = {
+            self: { href: baseURL },
+            stubs: { href: `${baseURL}/stubs` }
+        };
+
+        if (imposter.stubs) {
+            for (let i = 0; i < imposter.stubs.length; i += 1) {
+                imposter.stubs[i]._links = { self: { href: `${baseURL}/stubs/${i}` } };
+            }
+        }
     }
 
-    function toJSON (numberOfRequests, options) {
+    function toJSON (numberOfRequests, options = {}) {
         // I consider the order of fields represented important.  They won't matter for parsing,
         // but it makes a nicer user experience for developers viewing the JSON to keep the most
-        // relevant information at the top
-        const result = {
-                protocol: creationRequest.protocol,
-                port: server.port,
-                numberOfRequests: numberOfRequests
-            },
-            baseURL = `/imposters/${server.port}`,
+        // relevant information at the top. Some of the order of operations in this file represents
+        // that (e.g. keeping the _links at the end), and is tested with some documentation tests.
+        const result = createHeader(numberOfRequests, options),
             Q = require('q');
 
         options = options || {};
 
-        const promise = options.list ? Q(true) : addDetailsTo(result, baseURL);
-        return promise.then(() => {
-            result._links = {
-                self: { href: baseURL },
-                stubs: { href: `${baseURL}/stubs` }
-            };
+        if (options.list) {
+            addLinksTo(result);
+            return Q(result);
+        }
 
-            if (options.replayable) {
-                removeNonEssentialInformationFrom(result);
-            }
-            if (options.removeProxies) {
-                removeProxiesFrom(result);
-            }
+        const requestsPromise = options.replayable ? Q() : addRequestsTo(result);
+        return requestsPromise
+            .then(() => addStubsTo(result, options))
+            .then(() => {
+                if (options.replayable) {
+                    removeNonEssentialInformationFrom(result);
+                }
+                else {
+                    addLinksTo(result);
+                }
 
-            return removeFunctions(result);
-        });
+                if (options.removeProxies) {
+                    removeProxiesFrom(result);
+                }
+
+                return result;
+            });
     }
 
     return { toJSON };
