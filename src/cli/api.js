59a73d5ab72cb3520083de0b3bd896f7ea35419f
Brandon Byars <brandon.byars@gmail.com>
2020-12-25 20:20:48 -0600

    Modularized bin/mb for readability

---
+++ src/cli/api.js
@@ -0,0 +1,170 @@
+'use strict';
+
+const fs = require('fs'),
+    path = require('path'),
+    http = require('http'),
+    Q = require('q'),
+    ejs = require('ejs');
+
+function shouldLoadConfigFile (options) {
+    return typeof options.configfile !== 'undefined';
+}
+
+function putConfig (options, body) {
+    const deferred = Q.defer(),
+        requestOptions = {
+            method: 'PUT',
+            path: '/imposters',
+            port: options.port,
+            hostname: options.host || 'localhost',
+            headers: {
+                'Content-Type': 'application/json',
+                Connection: 'close'
+            }
+        },
+
+        request = http.request(requestOptions, response => {
+            response.body = '';
+            response.setEncoding('utf8');
+            response.on('data', chunk => { response.body += chunk; });
+            response.on('end', () => {
+                response.body = JSON.parse(response.body);
+                deferred.resolve(response);
+            });
+        });
+
+    request.on('error', deferred.reject);
+
+    request.write(body);
+    request.end();
+    return deferred.promise;
+}
+
+function getConfig (options) {
+    const deferred = Q.defer(),
+        requestOptions = {
+            method: 'GET',
+            path: '/imposters?replayable=true',
+            port: options.port,
+            hostname: options.host || 'localhost',
+            headers: {
+                'Content-Type': 'application/json',
+                Connection: 'close'
+            }
+        };
+
+    if (options.removeProxies) {
+        requestOptions.path += '&removeProxies=true';
+    }
+
+    const request = http.request(requestOptions, response => {
+        response.body = '';
+        response.setEncoding('utf8');
+        response.on('data', chunk => { response.body += chunk; });
+        response.on('end', () => {
+            deferred.resolve(response);
+        });
+    });
+
+    request.on('error', deferred.reject);
+
+    request.end();
+    return deferred.promise;
+}
+
+// usage: stringify(includeFile)
+// note: Trying to make this backwards compatible. However, the intent is to change
+// the signature to just require `includeFile`.
+function stringify (filename, includeFile, data) {
+    const resolvedPath = makePathInABackwardsCompatibleWay(filename, includeFile);
+    const contents = fs.readFileSync(resolvedPath, 'utf8'),
+        rendered = ejs.render(contents, {
+            data: data,
+            filename: CONFIG_FILE_PATH,
+            stringify: stringify,
+            inject: stringify // backwards compatibility
+        }),
+        jsonString = JSON.stringify(rendered.trim());
+
+    // get rid of the surrounding quotes because it makes the templates more natural to quote them there
+    return jsonString.substring(1, jsonString.length - 1);
+}
+
+function makePathInABackwardsCompatibleWay (filename, includeFile) {
+    var resolvedPath = null;
+    if (!includeFile) {
+        includeFile = filename;
+    }
+    resolvedPath = path.join(path.dirname(CONFIG_FILE_PATH), includeFile);
+    return resolvedPath;
+}
+
+function getContentsOrExit (file, server) {
+    try {
+        return fs.readFileSync(file, 'utf8');
+    }
+    catch (e) {
+        const message = e.code !== 'ENOENT' ? e : `No such file: ${file}`;
+        server.close(() => { });
+        console.error(message);
+        process.exit(1);
+        return '';
+    }
+}
+
+var CONFIG_FILE_PATH = null;
+function loadConfig (options, server) {
+    if (!shouldLoadConfigFile(options)) {
+        return Q(true);
+    }
+    CONFIG_FILE_PATH = options.configfile;
+    const configContents = getContentsOrExit(options.configfile, server),
+        parsedContents = options.noParse ? configContents : ejs.render(configContents, {
+            filename: options.configfile,
+            stringify: stringify,
+            inject: stringify // backwards compatibility
+        }),
+        json = JSON.parse(parsedContents),
+        // [json] Assume they left off the outer imposters array
+        imposters = json.imposters || [json];
+
+    return putConfig(options, JSON.stringify({ imposters: imposters }));
+}
+
+function logConnectionErrorAndExit (options, err) {
+    const host = options.host || 'localhost';
+    if (err.code === 'ECONNREFUSED') {
+        console.error(`No mb process running on http://${host}:${options.port}`);
+    }
+    else {
+        console.error(err);
+    }
+    process.exit(1);
+}
+
+function save (options) {
+    getConfig(options).then(response => {
+        fs.writeFileSync(options.savefile, response.body);
+    }).catch(ex => logConnectionErrorAndExit(options, ex)).done();
+}
+
+function replay (options) {
+    options.removeProxies = true;
+
+    getConfig(options).then(response => {
+        if (response.statusCode !== 200) {
+            console.error('Received status code ' + response.statusCode);
+            console.error(response.body);
+            process.exit(1);
+        }
+        else {
+            putConfig(options, response.body);
+        }
+    }).catch(ex => logConnectionErrorAndExit(ex, options)).done();
+}
+
+module.exports = {
+    loadConfig,
+    save,
+    replay
+};
---
+++ src/cli/cli.js
@@ -0,0 +1,402 @@
+'use strict';
+
+const fs = require('fs'),
+    yargs = require('yargs'),
+    aliases = {
+        d: 'debug',
+        h: 'host',
+        i: 'allowInjection',
+        l: 'localOnly',
+        m: 'mock',
+        p: 'port',
+        r: 'removeProxies',
+        w: 'ipWhitelist',
+        o: 'origin'
+    },
+    options = {
+        rcfile: {
+            description: 'the startup file ("run commands") containing configuration options',
+            nargs: 1,
+            type: 'string',
+            global: false
+        },
+        port: {
+            default: 2525,
+            description: 'the port to run the mountebank server on',
+            nargs: 1,
+            type: 'number',
+            alias: aliases.p,
+            global: false
+        },
+        host: {
+            description: 'the hostname to bind the mountebank server to',
+            nargs: 1,
+            type: 'string',
+            alias: aliases.h,
+            global: false
+        },
+        configfile: {
+            description: 'file to load imposters from, can be an EJS template',
+            nargs: 1,
+            type: 'string',
+            global: false
+        },
+        noParse: {
+            default: false,
+            description: 'prevent rendering the configfile as an EJS template',
+            type: 'boolean',
+            global: false
+        },
+        datadir: {
+            description: 'the database directory, useful for persistence and performance',
+            nargs: 1,
+            type: 'string',
+            global: false
+        },
+        pidfile: {
+            default: 'mb.pid',
+            description: 'where the pid is stored for the stop command',
+            nargs: 1,
+            type: 'string',
+            global: false
+        },
+        nologfile: {
+            default: false,
+            description: 'prevent logging to the filesystem',
+            type: 'boolean',
+            global: false
+        },
+        logfile: {
+            default: 'mb.log',
+            description: 'file used for all log',
+            nargs: 1,
+            type: 'string',
+            global: false
+        },
+        loglevel: {
+            default: 'info',
+            description: 'level for terminal logs',
+            nargs: 1,
+            type: 'string',
+            choices: ['debug', 'info', 'warn', 'error'],
+            global: false
+        },
+        log: {
+            description: 'customizable logging configuration',
+            nargs: 1,
+            type: 'string',
+            global: false
+        },
+        allowInjection: {
+            default: false,
+            description: 'set to allow JavaScript injection',
+            type: 'boolean',
+            alias: aliases.i,
+            global: false
+        },
+        localOnly: {
+            default: false,
+            description: 'allow connections only from localhost',
+            type: 'boolean',
+            alias: aliases.l,
+            global: false
+        },
+        ipWhitelist: {
+            default: '*',
+            description: 'pipe-delimited list of allowed IP address patterns to connect to the mountebank admin port',
+            type: 'string',
+            alias: aliases.w,
+            global: false
+        },
+        mock: {
+            default: false,
+            description: 'remember requests for mock verification',
+            type: 'boolean',
+            alias: aliases.m,
+            global: false
+        },
+        debug: {
+            default: false,
+            description: 'include stub match information in imposter retrievals for debugging purposes',
+            type: 'boolean',
+            alias: aliases.d,
+            global: false
+        },
+        savefile: {
+            default: 'mb.json',
+            description: 'file to save imposters to',
+            nargs: 1,
+            type: 'string',
+            global: false
+        },
+        protofile: {
+            default: 'protocols.json',
+            description: 'file to load custom protocol implementations from',
+            nargs: 1,
+            type: 'string',
+            global: false
+        },
+        removeProxies: {
+            default: false,
+            description: 'removes proxies from the configuration when using the save command',
+            type: 'boolean',
+            alias: aliases.r,
+            global: false
+        },
+        origin: {
+            default: false,
+            description: 'A safe origin for CORS requests',
+            type: 'string',
+            global: false
+        },
+        heroku: {
+            default: false,
+            type: 'boolean',
+            global: false
+        }
+    },
+    startOptions = {
+        port: options.port,
+        host: options.host,
+        configfile: options.configfile,
+        datadir: options.datadir,
+        noParse: options.noParse,
+        pidfile: options.pidfile,
+        nologfile: options.nologfile,
+        logfile: options.logfile,
+        loglevel: options.loglevel,
+        log: options.log,
+        allowInjection: options.allowInjection,
+        localOnly: options.localOnly,
+        ipWhitelist: options.ipWhitelist,
+        mock: options.mock,
+        debug: options.debug,
+        heroku: options.heroku,
+        protofile: options.protofile,
+        origin: options.origin
+    },
+    argv = yargs
+        .usage('Usage: mb [command=start] [options...]')
+        .command('start', 'Starts the server (default command)', startYargs => {
+            startYargs
+                .usage('Usage: mb [start] [options...]')
+                .help('help')
+                .wrap(null)
+                .options(startOptions)
+                .example('mb start --port 3000 --allowInjection --loglevel debug',
+                    'Starts on port 3000, allowing injections and turning on debug logging')
+                .example("mb --ipWhitelist '192.168.1.20|192.158.1.21'",
+                    'Starts on port 2525 with the given ip whitelist')
+                .epilog('Note on the last example that start is assumed if no command is specified\n\nFor more information, see http://www.mbtest.org/docs/commandLine');
+        })
+        .command('stop', 'Stops the server', stopYargs => {
+            stopYargs
+                .usage('Usage: mb stop [--pidfile file.pid]')
+                .help('help')
+                .wrap(null)
+                .options({ pidfile: options.pidfile })
+                .example('mb stop', 'Stops the process identified in mb.pid')
+                .example('mb stop --pidfile test.pid', 'Stops the process identified in test.pid')
+                .epilog('For more information, see http://www.mbtest.org/docs/commandLine');
+        })
+        .command('restart', "Stops the server if it's running and restarts it", restartYargs => {
+            restartYargs
+                .usage('Usage: mb restart [options...]')
+                .help('help')
+                .wrap(null)
+                .options(startOptions)
+                .example('mb restart --port 3000 --allowInjection --loglevel debug',
+                    'Restarts on port 3000, allowing injections and turning on debug logging')
+                .example('mb restart --pidfile test.pid', 'Stops the process identified in test.pid and restarts on port 2525')
+                .epilog('The port does not have to match the currently running instance, but the pidfile must match\n\nFor more information, see http://www.mbtest.org/docs/commandLine');
+        })
+        .command('save', 'Saves current imposter configuration to a config file', saveYargs => {
+            saveYargs
+                .usage('Usage: mb save [options...]\n\nSaves a snapshot of your configuration, including new responses created by proxies')
+                .help('help')
+                .wrap(null)
+                .options({
+                    port: options.port,
+                    savefile: options.savefile,
+                    removeProxies: options.removeProxies,
+                    host: options.host
+                })
+                .example('mb save --savefile config.json --removeProxies --port 3000',
+                    'Saves the config without proxies into config.json by querying port 3000')
+                .example('mb save', 'Saves the config as is into mb.json by querying port 2525')
+                .epilog('For more information, see http://www.mbtest.org/docs/commandLine');
+        })
+        .command('replay',
+            'Switches from record mode to replay by removing proxies',
+            replayYargs => {
+                replayYargs
+                    .usage('Usage: mb replay [--port 3000]')
+                    .help('help')
+                    .wrap(null)
+                    .options({ port: options.port, host: options.host })
+                    .example('mb replay --port 3000',
+                        'Resets the configuration of mountebank running on port 3000 to remove all proxies')
+                    .example('mb replay', 'Resets the configuration of mountebank running on port 2525 to remove all proxies')
+                    .epilog('For more information, see http://www.mbtest.org/docs/commandLine');
+            })
+        .version()
+        .wrap(null)
+        .epilog('mb help [command] provides more details\n\nFor more information, see http://www.mbtest.org/docs/commandLine')
+        .argv;
+
+function fixAliases (args) {
+    Object.keys(args).forEach(key => {
+        if (aliases[key]) {
+            args[aliases[key]] = args[key];
+            delete args[key];
+        }
+    });
+}
+
+// Unfortunately, while yargs defaults correctly when the command is present, I couldn't
+// figure out how to make it default when the command is absent, which I need to default to start.
+// You can set what yargs considers to be the default command by .command(['start', '*']), but
+// that changes the help docs in ways I don't want.
+function fixDefaults (args) {
+    if (args._.length === 0) {
+        Object.keys(startOptions).forEach(key => {
+            if (typeof args[key] === 'undefined') {
+                args[key] = startOptions[key].default;
+            }
+        });
+    }
+}
+
+function fixIPWhitelist (command, args) {
+    if (command === 'start' || command === 'restart') {
+        args.ipWhitelist = args.ipWhitelist.split('|');
+    }
+}
+
+function rawArgvIndexFor (key) {
+    // Use raw process.argv to ensure user actually passed the parameter, bypassing
+    // yargs defaulting. Also check alias.
+    let index = process.argv.indexOf(`--${key}`);
+    if (index < 0) {
+        const aliasKeys = Object.keys(aliases);
+        for (let i = 0; i < aliasKeys.length; i += 1) {
+            let alias = aliasKeys[i];
+            if (aliases[alias] === key) {
+                index = process.argv.indexOf(`-${alias}`);
+            }
+        }
+    }
+    return index;
+}
+
+function argIsPassedOnCLI (key) {
+    return rawArgvIndexFor(key) >= 0;
+}
+
+function argvOrDefaultFor (key) {
+    const index = rawArgvIndexFor(key);
+    if (index >= 0) {
+        return process.argv[index + 1];
+    }
+    else {
+        return options[key].default;
+    }
+}
+
+function parseLogConfiguration (args) {
+    // Not defaulted in yargs so we can test yargs value without defaulting interfering.
+    // This is needed for backwards compatibility with older CLI options.
+    const defaultConfig = {
+        level: 'info',
+        transports: {
+            console: {
+                colorize: true,
+                format: '%level: %message'
+            },
+            file: {
+                path: 'mb.log',
+                format: 'json'
+            }
+        }
+    };
+
+    if (typeof args.log === 'string') {
+        try {
+            args.log = JSON.parse(args.log);
+        }
+        catch (ex) {
+            console.error(`Cannot parse --log as JSON: ${ex}`);
+            args.log = defaultConfig;
+        }
+    }
+    else {
+        // Backwards compatibility with older CLI options. Using raw process.argv
+        // to ensure user actually passed the parameter, bypassing yargs defaulting
+        args.log = defaultConfig;
+        args.log.level = argvOrDefaultFor('loglevel');
+        args.log.transports.file.path = argvOrDefaultFor('logfile');
+        if (argIsPassedOnCLI('nologfile')) {
+            delete args.log.transports.file;
+        }
+    }
+
+    // Remove the old values to not confuse users retrieving configuration later
+    delete args.loglevel;
+    delete args.logfile;
+    delete args.nologfile;
+}
+
+// Prevent noise from being logged
+function removeNoise (args) {
+    delete args._;
+    delete args.$0;
+    delete args.version;
+}
+
+function addStartupFile (args) {
+    if (typeof args.rcfile !== 'string' || args.rcfile === '') {
+        return;
+    }
+    if (!fs.existsSync(args.rcfile)) {
+        console.error(`Cannot find rcfile ${args.rcfile}`);
+        return;
+    }
+    try {
+        const rc = JSON.parse(fs.readFileSync(args.rcfile));
+
+        // CLI options take priority over anything in the rcfile.
+        Object.keys(rc).forEach(key => {
+            if (!argIsPassedOnCLI(key)) {
+                args[key] = rc[key];
+            }
+        });
+    }
+    catch (ex) {
+        console.error(`Cannot parse rcfile ${args.rcfile}: ${ex}`);
+    }
+}
+
+function getCommandLineArgs (command, args) {
+    fixAliases(args);
+    fixDefaults(args);
+    fixIPWhitelist(command, args);
+    parseLogConfiguration(args);
+    removeNoise(args);
+    addStartupFile(args);
+    return args;
+}
+
+function error (message) {
+    console.error(`${message}\n`);
+    yargs.showHelp();
+    process.exit(1);
+}
+
+function help () {
+    yargs.showHelp();
+}
+
+const command = argv._.length === 0 ? 'start' : argv._[0],
+    args = getCommandLineArgs(command, argv);
+
+module.exports = { command, args, error, help };
