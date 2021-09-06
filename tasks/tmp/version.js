f5fb0a8f440f37ba74be05c9d766edc25d62c90f
Brandon Byars <brandon.byars@gmail.com>
2021-09-06 15:10:05 -0500

    Fixing web tests with nc executable path

--- tasks/tmp/version.js
+++ tasks/tmp/version.js
@@ -21,14 +21,9 @@ async function isReleaseVersion () {
 
 async function getVersion () {
     const isRelease = await isReleaseVersion(),
-        buildNumber = process.env.CIRCLE_BUILD_NUM,
-        passedVersion = process.env.MB_VERSION;
+        buildNumber = process.env.CIRCLE_BUILD_NUM;
 
-    if (typeof passedVersion !== 'undefined') {
-        // Used for passing the version between CI providers (e.g. CircleCI -> Travis)
-        return passedVersion;
-    }
-    else if (isRelease || typeof buildNumber === 'undefined') {
+    if (isRelease || typeof buildNumber === 'undefined') {
         // Leave as is if releasing or not in CI
         return current;
     }
