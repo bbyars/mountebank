aa31b2e54110b218a96befd8709217ddbcd22161
Brandon Byars <brandon.byars@gmail.com>
2021-09-11 15:59:50 -0500

    Removed all legacy build files

--- tasks/deploy/docs.js
+++ tasks/deploy/docs.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs'),
-    version = require('../../../package.json').version,
+    version = require('../../package.json').version,
     dir = `docs/mountebank/${version}`,
     execSync = require('child_process').execSync;
 
