0e95042c17451dd1f8e8292114b91a54a1a58c0b
Brandon Byars <brandon.byars@gmail.com>
2021-09-12 09:49:34 -0500

    Fixing merge conflicts

--- mbTest/api/http/httpProxyStubTest.js
+++ mbTest/api/http/httpProxyStubTest.js
@@ -8,7 +8,8 @@ const assert = require('assert'),
     port = api.port + 1,
     isWindows = require('os').platform().indexOf('win') === 0,
     timeout = isWindows ? 10000 : parseInt(process.env.MB_SLOW_TEST_TIMEOUT || 2000),
-    airplaneMode = process.env.MB_AIRPLANE_MODE === 'true';
+    airplaneMode = process.env.MB_AIRPLANE_MODE === 'true',
+    { HttpsProxyAgent } = require('hpagent');
 
 describe('http proxy stubs', function () {
     this.timeout(timeout);
