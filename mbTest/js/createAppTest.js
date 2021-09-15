1ff3826dcf45d6ac8708d03bd839e8c7a4408d0a
Brandon Byars <brandon.byars@gmail.com>
2021-09-15 05:02:22 -0500

    Used dynamic ports to avoid close() timeouts

--- mbTest/js/createAppTest.js
+++ mbTest/js/createAppTest.js
@@ -3,35 +3,27 @@
 const assert = require('assert'),
     express = require('express'),
     createApp = require('mountebank').createApp,
-    httpClient = require('../baseHttpClient').create('http'),
-    port = parseInt(process.env.MB_PORT || 2525);
-
-function listen (app) {
-    return new Promise((resolve, reject) => {
-        const server = app.listen(port, () => {
-            resolve({
-                close: function () {
-                    return new Promise(closeResolve => {
-                        server.close(() => {
-                            closeResolve();
-                        });
-                    });
-                }
-            });
-        });
-
-        server.on('error', e => reject(e));
-    });
-}
+    httpClient = require('../baseHttpClient').create('http');
 
 describe('Integration with existing server', function () {
-    this.timeout(20000);
+    this.timeout(10000);
 
     let server;
+    let port;
+
+    async function listen (app) {
+        return new Promise((resolve, reject) => {
+            server = app.listen(0, () => {
+                resolve(server.address().port);
+            });
+
+            server.on('error', e => reject(e));
+        });
+    }
 
     beforeEach(async function () {
         const app = express();
-        const mbApp = await createApp({});
+        const mbApp = await createApp({ debug: true });
 
         app.get('/', (req, res) => {
             res.status(200).send('ok');
@@ -41,12 +33,12 @@ describe('Integration with existing server', function () {
             mbApp(req, res);
         });
 
-        server = await listen(app);
+        port = await listen(app);
     });
 
     afterEach(async function () {
         await httpClient.del('/mountebank/imposters', port);
-        await server.close();
+        server.close(); // don't await; I failed to troubleshoot why this was timing out, hence dynamic ports
     });
 
     it('should success respond on application path', async function () {
@@ -58,19 +50,19 @@ describe('Integration with existing server', function () {
     it('should success respond on mountebank application path', async function () {
         const response = await httpClient.get('/mountebank/config', port);
 
-        assert.strictEqual(response.body.options.port, port);
+        assert.ok(response.body.options.debug);
     });
 
     it('should return create new imposter with consistent hypermedia', async function () {
-        const impostersPort = port + 1;
-        const creationResponse = await httpClient.post('/mountebank/imposters', { protocol: 'http', port: impostersPort }, port);
+        const creationResponse = await httpClient.post('/mountebank/imposters', { protocol: 'http' }, port);
+        const imposter = creationResponse.body;
 
-        assert.strictEqual(creationResponse.statusCode, 201, JSON.stringify(creationResponse.body, null, 2));
-        assert.strictEqual(creationResponse.headers.location, creationResponse.body._links.self.href);
+        assert.strictEqual(creationResponse.statusCode, 201, JSON.stringify(imposter, null, 2));
+        assert.strictEqual(creationResponse.headers.location, imposter._links.self.href);
 
-        const imposterResponse = await httpClient.get(`/mountebank/imposters/${impostersPort}`, port);
+        const imposterResponse = await httpClient.get(`/mountebank/imposters/${imposter.port}`, port);
 
         assert.strictEqual(imposterResponse.statusCode, 200);
-        assert.deepEqual(imposterResponse.body, creationResponse.body);
+        assert.deepEqual(imposter, creationResponse.body);
     });
 });
